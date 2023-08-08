d7b9a6c2d0efd94d1d202045f693a3d5be9b09d0
xadvent <piercestrutt@gmail.com>
2023-08-08 16:18:20 -0700

    Learned abt 'transition'. Changed btn animations

--- src/js/addButtonEvents.js
+++ src/js/addButtonEvents.js
@@ -7,13 +7,26 @@ const makeEvent = function (buttonID, makingFx) {
     const button = document.querySelector(`#${buttonID}`)
     button.onclick = function () {
         centerContent.innerHTML = ''
+        const getRidActive = (function () {
+            const listButton = document.querySelectorAll('.header-button')
+            listButton.forEach(x => x.id = '')
+            return
+        })()
+        this.id = 'active'
         makingFx()
+
     }
     return
+}
+const addActive = function () {
+    const activeHome = document.querySelector('#home')
+    return activeHome.id = 'active'
 }
 
 export default function () {
     makeEvent('menu', addMenuCards)
     makeEvent('home', homePage)
     makeEvent('contact', makeContact)
+    addActive()
+    return
 }
