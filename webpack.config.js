fc76d0bf19f339464a6ab10ee30daf8f091e65cd
xadvent <piercestrutt@gmail.com>
2023-08-08 17:17:40 -0700

    Changed mode for webpack. Changed tab name

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     module: {
         rules: [
