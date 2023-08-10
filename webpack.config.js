18c7f77e1f9cc69abcd45cce67d52c9619041f77
xadvent <piercestrutt@gmail.com>
2023-08-10 15:17:11 -0700

    Plugins minify CSS and JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin')
 const path = require('path');
+const { ExternalModule } = require('webpack');
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: 'index_bundle.js',
+        filename: 'index_bundle.min.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        esModule: false
+                    },
+                },
+                    'css-loader',
+                ],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -20,5 +32,20 @@ module.exports = {
             },
         ],
     },
-    plugins: [new HtmlWebpackPlugin()],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            '...',
+            new TerserPlugin({
+                terserOptions: {
+                    mangle: true,
+                }
+            }),
+            new CssMinimizerPlugin(),
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+    ],
 };
